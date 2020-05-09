3d46e98cb9836e9544dde19cefdee6dc4e865c26
--get <kr.vivek.kr@gmail.com>
2020-05-10 02:14:34 +0530

    youtube

--- src/Pages/Upload/Upload.js
+++ src/Pages/Upload/Upload.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Player } from 'video-react';
 
 export default function Upload() {
     return (
-        <div>
-            Upload
-        </div>
+    <Player>
+      <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+    </Player>
     )
 }
--- src/Pages/Videos/Videos.js
+++ src/Pages/Videos/Videos.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
+import YouTube from 'react-youtube';
 
-export default function Videos() {
-    return (
-        <div>
-            Videos
-        </div>
-    )
+export default class Videos extends Component {
+    render() {
+        const opts = {
+            height: '390',
+            width: '640',
+            playerVars: {
+              // https://developers.google.com/youtube/player_parameters
+              autoplay: 1,
+            },
+          };
+          return <YouTube videoId="_nBlN9yp9R8" opts={opts} onReady={this._onReady} />;
+        }
+       
+        _onReady(event) {
+          // access to player in all event handlers via event.target
+          event.target.pauseVideo();
+        }
 }
